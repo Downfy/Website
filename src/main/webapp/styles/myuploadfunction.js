701742254a9d447398ef6c70192e70e577e0c1dc
Tran Anh Tuan <tk1cntt@gmail.com>
2014-01-09 22:26:39 +0700

    Change category from id to url

--- src/main/webapp/styles/myuploadfunction.js
+++ src/main/webapp/styles/myuploadfunction.js
@@ -2,15 +2,28 @@ $(function() {
     $('#appIconButton').click(function() {
         $('#progress .progress-bar').removeAttr('style');
     });
-    $('#appFileUpload').fileupload({
+    $('#appIconFileUpload').fileupload({
         dataType: 'json',
         done: function(e, data) {
             $('#appIconImage').removeClass('img-thumbnail');
             $('#appIconImage').addClass('img-circle');
             $('#appIconInput').val(data.result.fileName);
-            $.each(data.result, function(index, file) {
-                $('#appIconInput').val(file.fileName);
-            });
+        },
+        progressall: function(e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
+            $('#progress .progress-bar').css(
+                    'width',
+                    progress + '%'
+                    );
+        }
+    });
+
+    $('#appAPKFileUpload').fileupload({
+        dataType: 'json',
+        done: function(e, data) {
+            $('#appIconImage').removeClass('img-thumbnail');
+            $('#appIconImage').addClass('img-circle');
+            $('#appIconInput').val(data.result.fileName);
         },
         progressall: function(e, data) {
             var progress = parseInt(data.loaded / data.total * 100, 10);
