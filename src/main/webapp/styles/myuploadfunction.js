b76ff8734587abeb573ca4ce29298c9e8fceadb6
Tran Anh Tuan <tk1cntt@gmail.com>
2014-01-21 17:18:32 +0700

    Add support Csrf Header when upload

--- src/main/webapp/styles/myuploadfunction.js
+++ src/main/webapp/styles/myuploadfunction.js
@@ -1,4 +1,9 @@
 $(function() {
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+    $(document).ajaxSend(function(e, xhr, options) {
+        xhr.setRequestHeader(header, token);
+    });
     $('#appIconButton').click(function() {
         $('#progress .progress-bar').removeAttr('style');
     });
