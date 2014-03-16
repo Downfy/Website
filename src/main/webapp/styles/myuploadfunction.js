11764b253f4ee14be3841845f008003fc98d091b
tuanta <tuanta@tuanta-laptop>
2014-03-17 00:11:26 +0700

    Feature: Upload apk

--- src/main/webapp/styles/myuploadfunction.js
+++ src/main/webapp/styles/myuploadfunction.js
@@ -13,7 +13,7 @@ $(function() {
             $('#appIconImage').removeClass('img-thumbnail');
             $('#appIconImage').addClass('img-circle');
             $('#appIconInput').val(data.result.fileName);
-            $('#appIconImage').attr("src", '/resources' + data.result.fileName);
+            $('#appIconImage').attr("src", '/mystore/resources' + data.result.fileName);
         },
         progressall: function(e, data) {
             var progress = parseInt(data.loaded / data.total * 100, 10);
@@ -27,9 +27,12 @@ $(function() {
     $('#appAPKFileUpload').fileupload({
         dataType: 'json',
         done: function(e, data) {
-            $('#appIconImage').removeClass('img-thumbnail');
-            $('#appIconImage').addClass('img-circle');
-            $('#appIconInput').val(data.result.fileName);
+            if (data.result.fileName !== ""){
+                $('#appAPKInput').val(data.result.fileName);
+                $('#appAPKSizeInput').val(data.result.fileSize);
+                $('#add-application-form').append("Version (format x.x.x): <input type=\"text\" class=\"form-control\" style=\"width: 100px\" name=\"appVersion\" value=\"1.0.0\" />");
+                $('#add-application-form').append("<br/><button type=\"submit\" class=\"btn btn-primary\">Upload App</button>");
+            }
         },
         progressall: function(e, data) {
             var progress = parseInt(data.loaded / data.total * 100, 10);
